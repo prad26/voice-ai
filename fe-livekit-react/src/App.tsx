d75d27a6e94a9b0008cbcbe6a8f0e66ade956169
Pradyumna <pradyumna.banakar@gmail.com>
2025-07-25 17:21:16 +0900

    add inital react frontend

---
+++ fe-livekit-react/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <p className='text-xl'>This a LiveKit Client built in React</p>
+    </div>
+  );
+}
+
+export default App;
