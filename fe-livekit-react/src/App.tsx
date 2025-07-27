1dca01a2dbb411dfed299a3a3816754e0168f947
Pradyumna <pradyumna.banakar@gmail.com>
2025-07-27 17:24:29 +0900

    adjust frontend workflow

--- fe-livekit-react/src/App.tsx
+++ fe-livekit-react/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-      <p className='text-xl'>This a LiveKit Client built in React!</p>
+      <p className='text-xl'>This a LiveKit Client built in React!!</p>
     </div>
   );
 }
